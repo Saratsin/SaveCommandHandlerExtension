da07a13e5c5dd8a0afff87a2d666920d4fad7cf8
Taras Shevchuk <schew4uk@gmail.com>
2020-01-02 19:09:48 +0200

    SaveCommandHandlerExtension implementation

---
+++ SaveCommandHandlerExtension.Mac/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "SaveCommandHandlerExtension",
+    Namespace = nameof(SaveCommandHandlerExtension),
+    Version = "1.0"
+)]
+
+[assembly: AddinName("SaveCommandHandlerExtension")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("SaveCommandHandlerExtension")]
+[assembly: AddinAuthor("Taras Shevchuk")]
---
+++ SaveCommandHandlerExtension/SaveCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+
+namespace SaveCommandHandlerExtension
+{
+    [Export(typeof(ICommandHandler))]
+    [Name(nameof(SaveCommandHandler))]
+    [ContentType(StandardContentTypeNames.Code)]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    public class SaveCommandHandler : ICommandHandler<SaveCommandArgs>
+    {
+        public string DisplayName => nameof(SaveCommandHandler);
+
+        private readonly IEditorCommandHandlerServiceFactory _editorCommandHandlerServiceFactory;
+
+        [ImportingConstructor]
+        public SaveCommandHandler(IEditorCommandHandlerServiceFactory editorCommandHandlerServiceFactory)
+        {
+            _editorCommandHandlerServiceFactory = editorCommandHandlerServiceFactory;
+        }
+
+        public bool ExecuteCommand(SaveCommandArgs args, CommandExecutionContext executionContext)
+        {
+            try
+            {
+                var service = _editorCommandHandlerServiceFactory.GetService(args.TextView);
+                Debug.WriteLine($"I am executing something on save with {service.GetType()}");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            return true;
+        }
+
+        public CommandState GetCommandState(SaveCommandArgs args)
+        {
+            return CommandState.Available;
+        }
+
+    }
+}
